f95050f0f47add4cbff6e7b12542f9e6cb793d08
DevHimanshu63 <hsc95067@gmail.com>
2022-02-08 11:21:15 +0530

    create base.html for every html file

--- blogApp/views.py
+++ blogApp/views.py
@@ -1,9 +1,11 @@
+import re
 from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
 def bloghome(request): 
-    return HttpResponse("bloghome")
+    return render(request,'blog/bloghome.html')
 
 def blogpost(request, slug):
-    return HttpResponse(f"blogpost {slug}")
+    # return HttpResponse(f"blogpost {slug}")
+    return render(request,'blog/blogpost.html')
