b9a7150cc29d90dc088e28e5e2d9cf8509f8313a
DevHimanshu63 <hsc95067@gmail.com>
2022-02-08 12:52:52 +0530

    contact information save in admin pannel database

--- Home/models.py
+++ Home/models.py
@@ -12,3 +12,6 @@ class Contact(models.Model):
     phone=models.CharField(max_length=100)
     content=models.TextField()
     Timestamp=models.DateTimeField(auto_now_add=True, blank=True)
+
+    def __str__(self):
+        return self.Name
--- Home/views.py
+++ Home/views.py
@@ -2,10 +2,19 @@ import re
 from django.http import HttpResponse
 from django.shortcuts import render
 
+from Home.models import Contact
+
 # Create your views here.
 def home(request):
     return render(request,'home/home.html')
 def about(request):
     return HttpResponse("about")
 def contact(request):
+    if request.method=='POST':
+        name=request.POST['name']
+        email=request.POST['email']
+        phone=request.POST['phone']
+        content=request.POST['content']
+        user=Contact(Name=name,email=email,phone=phone,content=content)
+        user.save()
     return render(request,'home/contact.html')
